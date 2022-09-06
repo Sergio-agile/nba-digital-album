c6552152b9f356972f1b4e327ba43d9a3ededcab
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2022-09-06 17:54:49 +0200

    started landingpage design

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'albums/cards'
   get 'albums/quizzes'
   devise_for :users
-  root to: "pages#home"
+  #root to: "pages#home"
+  root to: "pages#landing"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
