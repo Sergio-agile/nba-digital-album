2103b9d6559e788861adff9380f53cb87860e4c8
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2022-09-09 11:19:04 +0200

    Landing page slide and Navbar done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   root to: "pages#landing"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "albums", to: "albums#index"
+  get "viewing_albums", to: "pages#viewing_albums"
   #get 'albums/cards'
   #get 'albums/quizzes'
 end
