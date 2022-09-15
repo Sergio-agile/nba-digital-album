b1b1b491c655cb353490919d2d2ae04fe8ffcbc9
Florian Cywinski <florian.cywinski@protonmail.com>
2022-09-15 13:24:28 +0200

    color-quizzes

--- app/javascript/controllers/quiz_controller.js
+++ app/javascript/controllers/quiz_controller.js
@@ -22,9 +22,9 @@ export default class extends Controller {
             // button.style.backgroundColor = "#007A33";
             const quizResultMessage = document.getElementById("quiz-result-message");
             quizResultMessage.innerText = "Congratulations! You're anwer is correct! You got a new pack of 5 cards! We're adding this cards to your album next week :)";
-            quizResultMessage.style.backgroundColor = "#AC4034";
+            quizResultMessage.style.backgroundColor = "#F4A460";
             // quizResultMessage.style.backgroundColor = "#AA76D8";
-            quizResultMessage.style.opacity = 0.8;
+            quizResultMessage.style.opacity = 0.7;
             quizResultMessage.style.display = "block";
             const quizOpenPack = document.getElementById("quiz-open-pack").style.display = "block";
         } else {
