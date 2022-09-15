adc1b4af1090fc9b1bac4982d1df54df6e225218
Luke Bullen <lukeabullen@gmail.com>
2022-09-15 16:53:25 +0100

    quizes changes in colors

--- app/javascript/controllers/quiz_controller.js
+++ app/javascript/controllers/quiz_controller.js
@@ -18,23 +18,23 @@ export default class extends Controller {
         const button = this.buttonTarget
         console.log(button);
         if (this.correctValue == "correct") {
-            button.style.backgroundColor = "#3e753b";
+            button.style.backgroundColor = "#3e753b"; //green
             // button.style.backgroundColor = "#007A33";
             const quizResultMessage = document.getElementById("quiz-result-message");
-            quizResultMessage.innerText = "Congratulations! Your answer is correct! You won a new pack of 5 cards!";
-            quizResultMessage.style.backgroundColor = "#F4A460";
+            quizResultMessage.innerText = "Congratulations! You won a new pack of 5 cards!";
+            quizResultMessage.style.backgroundColor = "rgba(62, 117, 59, 0.7)"; //green
             // quizResultMessage.style.backgroundColor = "#AA76D8";
-            quizResultMessage.style.opacity = 0.7;
+            // quizResultMessage.style.opacity = 0.7;
             quizResultMessage.style.display = "block";
             const quizOpenPack = document.getElementById("quiz-open-pack").style.display = "block";
         } else {
-            button.style.backgroundColor = "#B22222";
+            button.style.backgroundColor = "#C8102E"; //$red
             // button.style.backgroundColor = "#C8102E";
             const quizResultMessage = document.getElementById("quiz-result-message");
             quizResultMessage.innerText = "Sooooo cloze!!! Best luck next time";
-            quizResultMessage.style.backgroundColor = "#F4A460";
+            quizResultMessage.style.backgroundColor = "rgba(200, 16, 46, 0.7)"; //$red
             // quizResultMessage.style.backgroundColor = "#AA76D8";
-            quizResultMessage.style.opacity = 0.7;
+            // quizResultMessage.style.opacity = 0.7;
             quizResultMessage.style.display = "block";
             document.getElementById("quiz-try-another").style.display = "block";
         }
